4b779d9213e2559d25a1929638635c570680f25c
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2021-01-11 22:08:44 +0530

    Create SinglyLinkedList.py

---
+++ SinglyLinkedList.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+    def LinkedList(self,data,head):
+        self.head=None
+        node=Node(data,head)
+        self.head=node
+    
