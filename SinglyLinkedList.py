921bc8ddc65303911cd99eb5722f0cef5810d77e
Ash515 <aaashwin515@gmail.com>
2021-05-23 19:09:12 +0530

    added binary search tree

--- SinglyLinkedList.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-    def LinkedList(self,data,head):
-        self.head=None
-        node=Node(data,head)
-        self.head=node
-    
