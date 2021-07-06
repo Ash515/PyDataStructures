b64fad11e5c9bb48f9d2247b44a59a80d349b774
Ash515 <aaashwin515@gmail.com>
2021-07-06 21:36:11 +0530

    Added heap datastructure

--- Linear Structures/HashTable/HashTable.py
+++ Linear Structures/HashTable/HashTable.py
@@ -1,3 +1,11 @@
+'''
+In python Dictionaries are useful for making hashtables
+We can create a hash function in various methods
+here we are going to see how to change a given key into hashing based on ASCII
+characters.
+'''
+
+
 class HashTable():
     def __init__(self):
         self.Max=100
@@ -9,7 +17,24 @@ class HashTable():
             s=ord(i)
             h+=s
             return h % self.Max
+    
+    def add(self,key,val):  # adding value to the key in a dictionary
+        f=self.get_hash(key)
+        self.array[f]=val
+    
+    def get(self,key):
+        g=self.get_hash(key)
+        return self.array[g]
+
+
+
       
 
 t=HashTable()
-print(t.get_hash('march 6'))
+print(t.get_hash('october 6'))
+t.add('october 6',250)
+print(t.array)
+
+get_val=t.get('october 6')
+print(get_val)
+
