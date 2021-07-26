167457c4534fe4a8b6483cedfb0724d18629b9d8
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 <aaashwin515@gmail.com>
2021-07-26 21:51:17 +0530

    added deletion edge operation in adjacency list

--- Non Linear Structures/Graphs/Deletion/Adjacency List/Directed and Unweighted graph/Deletion of Edges.py
+++ Non Linear Structures/Graphs/Deletion/Adjacency List/Directed and Unweighted graph/Deletion of Edges.py
@@ -10,18 +10,17 @@ def add_edge(v1,v2):
     elif v2 not in graph:
         print(v2,"not present in graph")
     else:
+        
         graph[v1].append(v2)
-        graph[v2].append(v1)
 
-def delete_node(v):
-    if v not in graph:
-        print(v, "not present in graph")
+def delete_edge(v1,v2):
+    if v1 not in graph:
+        print(v1,"not present in graph")
+    elif v2 not in graph:
+         print(v2,"not present in graph")
     else:
-        graph.pop(v)
-        for i in graph:
-            list1=graph[i]
-            if v in list1:
-                list1.remove(v)
+        if v2 in graph[v1]:
+            graph[v1].remove(v2)   
 
 
 graph={}
@@ -29,14 +28,10 @@ add_node('A')
 add_node('B')
 add_node('C')
 add_node('D')
+add_node('E')
 add_edge('A','D')
 add_edge('A','C')
 add_edge('A','B')
-add_edge('A','E') # E not present in graph
-delete_node('A')
+add_edge('D','A') # E not present in graph
+delete_edge('A','D')
 print(graph)
-
-
-
-       
-
--- Non Linear Structures/Graphs/Deletion/Adjacency List/Undirected and Unweighted graph/Deletion of Edges.py
+++ Non Linear Structures/Graphs/Deletion/Adjacency List/Undirected and Unweighted graph/Deletion of Edges.py
@@ -13,15 +13,18 @@ def add_edge(v1,v2):
         graph[v1].append(v2)
         graph[v2].append(v1)
 
-def delete_node(v):
-    if v not in graph:
-        print(v, "not present in graph")
+
+def delete_edge(v1,v2):
+    if v1 not in graph:
+        print(v1,"not present in graph")
+    elif v2 not in graph:
+         print(v2,"not present in graph")
     else:
-        graph.pop(v)
-        for i in graph:
-            list1=graph[i]
-            if v in list1:
-                list1.remove(v)
+        if v2 in graph[v1]:
+            graph[v1].remove(v2)
+            graph[v2].remove(v1)
+
+        
 
 
 graph={}
@@ -31,9 +34,9 @@ add_node('C')
 add_node('D')
 add_edge('A','D')
 add_edge('A','C')
-add_edge('A','B')
-add_edge('A','E') # E not present in graph
-delete_node('A')
+add_edge('C','D')
+add_edge('C','E') 
+delete_edge('C','D') #Op=>> {'A': ['D', 'C'], 'B': [], 'C': ['A'], 'D': ['A']}
 print(graph)
 
 
